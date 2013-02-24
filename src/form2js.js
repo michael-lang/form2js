66c7746cdd67dcaf46a5d14379813bee1766faec
Michael Lang <michael.lang@nexul.com>
2013-02-24 15:22:43 -0600

    added nuget package build support
    from command line, type 'release 1 0 0' for release 1.0.0

--- src/form2js.js
+++ src/form2js.js
@@ -1,6 +1,7 @@
 /**
  * Copyright (c) 2010 Maxim Vasiliev
- * Copyright 2012, AUTHORS.txt
+ * Copyright (c) @ReleaseCurrentYear, Michael Lang
+ * Version @ReleaseVersionNumber
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
--- src/js2form.js
+++ src/js2form.js
@@ -1,5 +1,7 @@
 /**
  * Copyright (c) 2010 Maxim Vasiliev
+ * Copyright (c) @ReleaseCurrentYear, Michael Lang
+ * Version @ReleaseVersionNumber
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
